1e1f19c6915ae5903a38ff1790733bd922ab4404
Khushboo Mittal <1154khb@gmail.com>
2025-06-12 21:32:47 +0530

    update summary and test

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,8 +4,30 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
+
     @Test
-    public void testAdd() {
+    public void testAddPositiveNumbers() {
         assertEquals(5, App.add(2, 3));
+    }
+
+    @Test
+    public void testAddNegativeNumbers() {
+        assertEquals(-5, App.add(-2, -3));
+    }
+
+    @Test
+    public void testAddZero() {
+        assertEquals(0, App.add(0, 0));
+    }
+
+    @Test
+    public void testAddPositiveAndNegative() {
+        assertEquals(1, App.add(3, -2));
+    }
+
+    @Test
+    public void testAddFailureExample() {
+        // This test is intentionally incorrect to simulate a failure
+        assertEquals(10, App.add(2, 2));
     }
 }
